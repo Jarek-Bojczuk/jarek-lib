c01ee12c11eabfca89648fd4f49d0d2d5f08c40e
Jarek-Bojczuk <jarek.bojczuk@crypto.com>
2022-06-01 14:47:04 +0200

    missing file

---
+++ tests/fdt/test.py
@@ -0,0 +1,14 @@
+import unittest
+from src.fdt.utils import my_first_lib_method
+
+
+class FirstTest(unittest.TestCase):
+
+    def setUp(self):
+        super().setUp()
+        print('Setup tests')
+
+    def test_first_test_case(self):
+        print('Transformation test')
+        res = my_first_lib_method()
+        self.assertEqual(res, "my_first_lib_method")
